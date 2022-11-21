0899ecfa2dc9e24ecf896e6e62e0605afbffd676
saeed nourian <saeednourian82@gmail.com>
2022-11-21 23:16:49 +0330

    new method to find the name of bank using beautiful soup fo data of www.elmefarda.com/پیش-شماره-کارت-بانکی/

--- main.py
+++ main.py
@@ -2,16 +2,16 @@ from ctypes import ArgumentError
 from requests import get 
 from bs4 import BeautifulSoup 
 from random import choice, randint 
-
-
+ 
+ 
 class Card: 
-    def init(self, number=10**15) -> None: 
+    def __init__(self, number=10**15) -> None: 
         if len(str(number)) != 16 or type(number) != int: 
             raise ArgumentError('Number should be an inetger with 16 digits.') 
         self.n = number 
         self.valid = self.check() 
         self.bank = self.id_bank() 
-
+ 
     def check(self) -> bool: 
         n = str(self.n) 
         # sums = sum([i * j if i * j < 10 else j * i - 9 for i, j in ((int(x), int(y)) for x, y in zip(n, '21' * 8))]) 
@@ -21,4 +21,7 @@ class Card:
             if (l) > 9: 
                 l -= 9 
             sums += l 
-        return not sums % 10
+        return not sums % 10 
+ 
+    def id_bank(self):
+        pass
