2fdb12cdaccd21794d5c488338007aca6213d7b6
saeed nourian <saeednourian82@gmail.com>
2022-11-21 23:09:58 +0330

    main

---
+++ main.py
@@ -0,0 +1,13 @@
+from ctypes import ArgumentError 
+from requests import get 
+from bs4 import BeautifulSoup 
+from random import choice, randint 
+ 
+ 
+class Card: 
+    def __init__(self, number=10**15) -> None: 
+        if len(str(number)) != 16 or type(number) != int: 
+            raise ArgumentError('Number should be an inetger with 16 digits.') 
+        self.n = number 
+        self.valid = self.check() 
+        self.bank = self.id_bank()
